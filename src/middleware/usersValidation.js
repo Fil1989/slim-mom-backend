327a8755771a6efed9435190316b1b980fa7d771
Dema Maksim <maksimdema@gmail.com>
2021-07-25 15:17:10 +0300

    add validate search

--- src/middleware/usersValidation.js
+++ src/middleware/usersValidation.js
@@ -6,7 +6,7 @@ const validateAuth = (req, res, next) => {
     email: Joi.string()
       .email({
         minDomainSegments: 2,
-        tlds: { allow: ['com', 'net'] },
+        tlds: { allow: ['com', 'net', 'ua'] },
       })
       .required(),
     password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
--- src/routes/api/productsRoutes.js
+++ src/routes/api/productsRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const authMiddleware = require('../../middleware/authMiddleware')
+const { validateSearch } = require('../../middleware/productsValidation')
 const { search } = require('../../controllers/productsControllers')
 
-router.get('/search/:product', authMiddleware, search)
+router.get('/search/:product', authMiddleware, validateSearch, search)
 
 module.exports = router
