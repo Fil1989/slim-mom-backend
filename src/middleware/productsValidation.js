7074e80c5aa41f3422f9c73f3ba06212edaa480d
Filip Koza <peacefilip1989@gmail.com>
2021-07-27 17:55:23 +0200

    опис в описі
    валідація для запиту по даті й заміна growth на height

--- src/middleware/productsValidation.js
+++ src/middleware/productsValidation.js
@@ -15,6 +15,21 @@ const validateSearch = (req, res, next) => {
     next()
   }
 }
+const validateDate = (req, res, next) => {
+  const schemaDate = Joi.object({
+    date: Joi.string().required().pattern(new RegExp('^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]$')),
+  })
+
+  const validation = schemaDate.validate(req.params)
+
+  if (validation.error) {
+    const [{ message }] = validation.error.details
+
+    return res.status(400).json({ message: `field ${message.replace(/"/g, '')}` })
+  } else {
+    next()
+  }
+}
 
 const validateAddProduct = (req, res, next) => {
   const schemaAuth = Joi.object({
@@ -34,4 +49,4 @@ const validateAddProduct = (req, res, next) => {
   }
 }
 
-module.exports = { validateSearch, validateAddProduct }
+module.exports = { validateSearch, validateDate, validateAddProduct }
--- src/services/calcService.js
+++ src/services/calcService.js
@@ -1,9 +1,9 @@
 const User = require('../schema/userSchema')
 
 const calculate = async body => {
-  const { weight, growth, age, desiredWeight } = body
+  const { weight, height, age, desiredWeight } = body
   const result = Math.round(
-    10 * Number(weight) + 6.25 * Number(growth) - 5 * age - 161 - 10 * (Number(weight) - Number(desiredWeight)),
+    10 * Number(weight) + 6.25 * Number(height) - 5 * age - 161 - 10 * (Number(weight) - Number(desiredWeight)),
   )
 
   return result
